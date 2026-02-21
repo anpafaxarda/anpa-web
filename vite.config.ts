395ad2cfa04f7ff696090b1800716effebe059fc
Alfonso Madrid <alfonso.madrid.dev@gmail.com>
2026-02-21 21:34:52 +0100

    Cambiada configuración de vercel

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig(({ mode }) => ({
     analog({
       static: true,
       nitro: {
-        preset: 'vercel-static',
+        preset: 'vercel',
       },
       prerender: {
         routes: [
