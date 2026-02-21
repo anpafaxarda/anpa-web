82a00aea47df0df5d638b51b75be83bc2c5e8eee
ANPA A Faxarda <anpafaxarda@gmail.com>
2026-02-21 19:57:25 +0100

    Fix/public folder (#19)
    * Arreglado navegación y finalizadas otras secciones
    
    * Añadido public folder
    
    ---------
    
    Co-authored-by: Alfonso Madrid <alfonso.madrid.dev@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  publicDir: 'public',
   build: {
     target: ['es2020'],
   },
